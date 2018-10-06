a95d38d9dac15c3587ea584fbcdc4589c307a8d5
Willovent <klein.william@outlook.com>
2018-10-06 14:16:50 +0200

    feat(content): add some content (todo-list)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'server-side-rendering';
+export class AppComponent implements OnInit {
+  public todos: Todo[] = [];
+  public newTodo: string;
+
+
+  ngOnInit() {
+    this.todos.push({ name: 'Faire une super présentation', check: true });
+    this.todos.push({ name: 'Comparer JIT vs AOT', check: true });
+    this.todos.push({ name: 'Parler des features modules', check: true });
+    this.todos.push({ name: 'Comparer les stratégies de detection du changement', check: true });
+    this.todos.push({ name: 'Présenter server-side rendering', check: false });
+  }
+
+  addTodo() {
+    this.todos.push({ name: this.newTodo, check: false });
+    this.newTodo = '';
+  }
+
+}
+
+export interface Todo {
+  name: string;
+  check: boolean;
 }
